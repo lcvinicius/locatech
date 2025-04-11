3170a0525db31aaa32f17972cce773d817c2137a
Lucas Vinícius <lucasvsn19@gmail.com>
2025-04-11 00:21:29 -0300

    feat: add endpoints to get vehicles by id and paginated list

--- src/main/java/br/com/fiap/locatech/locatech/controllers/VehicleController.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/VehicleController.java
@@ -1,17 +1,52 @@
 package br.com.fiap.locatech.locatech.controllers;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.locatech.locatech.services.VehicleService;
+import ch.qos.logback.classic.Logger;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+import br.com.fiap.locatech.locatech.entities.Vehicle;
+
+
 
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
 
-    	
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(VehicleController.class);
+
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+    
+    @GetMapping("")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     @GetMapping
-    public String getVehicles() {
-        return "List of vehicles";
+    public ResponseEntity<List<Vehicle>> findAllVehicles( @RequestParam int page, @RequestParam int size){
+        logger.info("Finding all vehicles with page: {} and size: {}", page, size);
+        List<Vehicle> vehicles = vehicleService.findAllVehicles(page, size);
+        return ResponseEntity.ok(vehicles);
+        
+    }
+    @GetMapping("{id}")
+    public ResponseEntity <Optional<Vehicle>> findVehicleById (@PathVariable ("id") Long id){
+        logger.info("/vehicles/" + id);
+        var vehicle = this.vehicleService.findVehicleById(id);
+        return ResponseEntity.ok(vehicle);
     }
     
 }
---
+++ src/main/java/br/com/fiap/locatech/locatech/services/VehicleService.java
@@ -0,0 +1,49 @@
+package br.com.fiap.locatech.locatech.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import br.com.fiap.locatech.locatech.entities.Vehicle;
+import br.com.fiap.locatech.locatech.repositories.VehicleRepository;
+
+@Service
+public class VehicleService {
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    public List<Vehicle> findAllVehicles(int page, int size) {
+        int offSet = (page - 1) * size;
+        return this.vehicleRepository.findAll(size, offSet);
+
+    }
+
+    public  Optional<Vehicle> findVehicleById(Long id) {
+        return this.vehicleRepository.findById(id);
+    }
+
+    public void saveVehicle(Vehicle vehicle) {
+        var save = this.vehicleRepository.save(vehicle);
+        Assert.state(save == 1, "Error saving vehicle"
+        + " - " + vehicle.getModel());
+    }
+
+    public void updateVehicle(Vehicle vehicle, Long id) {
+        var update = this.vehicleRepository.update(vehicle, id);
+        Assert.state(update == 1, "Error updating vehicle"
+        + " - " + vehicle.getModel());
+    }
+    
+    public void deleteVehicle(Long id) {
+        var delete = this.vehicleRepository.delete(id);
+        Assert.state(delete == 1, "Error deleting vehicle"
+        + " - " + id);
+
+    }
+
+}
